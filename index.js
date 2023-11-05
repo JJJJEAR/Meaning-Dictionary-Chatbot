890bcc43d7b9c94f224dd7ddf9e3e6f68d89a1f8
JJJJEAR <s6303051623152@email.kmutnb.ac.th>
2023-11-05 10:12:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const env = dotenv.config().parsed;
 const axios = require('axios').default;
 const app = express();
 const langdetect = require('langdetect');
-const serverless = require('serverless-http');
+// const serverless = require('serverless-http');
 
 const lineConfig = {
   channelAccessToken: env.ACCESS_TOKEN,
